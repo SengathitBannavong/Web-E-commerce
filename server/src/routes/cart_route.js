02791de7600dd9d67e5c2aff9d0266dde30577d0
Bannavong <bannavong.sa239717@sis.hust.edu.vn>
2025-11-19 20:26:09 +0700

    Fix API for Correctly Principle (#8)
    - fix api of User route for correcly principles
    - fix api Product route and Product controller for correcly principles
    - fix api Order route and Order controller for correcly principles
    - handle order items same api
    - fix api Cart route and Cart controller for correcly principles

--- server/src/routes/cart_route.js
+++ server/src/routes/cart_route.js
@@ -2,30 +2,24 @@ import express from "express";
 import {
   create_cart,
   delete_cart,
-  get_all_cart_items,
-  get_all_carts,
-  get_all_details_cart_by_user_id,
-  get_cart_by_id,
-  get_cart_by_user_id,
+  get_carts,
   update_cart
 } from "../controllers/cart_controller.js";
 
 const cart_router = express.Router();
 // Cart API routes
-cart_router.get("/view-all-carts", get_all_carts);
-cart_router.get("/view-cart/:id", get_cart_by_id);                                        // url sent pass parameter
-cart_router.get("/view-cart", get_cart_by_id);                                            // url sent pass query  
-cart_router.get("/view-cart-by-user/:userId", get_cart_by_user_id);                       // url sent pass parameter
-cart_router.get("/view-cart-by-user", get_cart_by_user_id);                               // url sent pass query
-cart_router.get("/view-detailed-cart-by-user/:userId", get_all_details_cart_by_user_id);  // url sent pass parameter
-cart_router.get("/view-detailed-cart-by-user", get_all_details_cart_by_user_id);          // url sent pass query
-cart_router.post("/create-cart", create_cart);
-cart_router.delete("/delete-cart/:id/:auth", delete_cart);                                // url sent pass parameter
-cart_router.delete("/delete-cart", delete_cart);                                          // url sent pass query
-cart_router.put("/update-cart/:id", update_cart);                                         // url sent pass parameter
-cart_router.put("/update-cart", update_cart);                                             // url sent pass query
+cart_router.get("/", get_carts);
+cart_router.get("/:id", get_carts);
+cart_router.get("/:userId", get_carts);
+cart_router.get("/:type/:userId", get_carts);
 
-// Cart Items routes
-cart_router.get("/view-all-cart-items", get_all_cart_items);
+cart_router.post("/", create_cart);
+
+cart_router.delete("/", delete_cart);
+cart_router.delete("/:id/:auth", delete_cart);
+
+cart_router.put("/", update_cart);
+cart_router.put("/:id", update_cart);
+
 
 export { cart_router };
