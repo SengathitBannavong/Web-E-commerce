c4c787dc1d519ce4f9a41a19c7d04f2a0b3ba1e8
Bannavnog <bannavong.sa239717@sis.hust.edu.vn>
2025-12-06 14:54:47 +0700

    - update cart router and controller

--- server/src/routes/cart_route.js
+++ server/src/routes/cart_route.js
@@ -1,25 +1,33 @@
 import express from "express";
 import {
+  // Cart functions
   create_cart,
+  // Cart Item functions
+  create_cart_item,
+  delete_all_cart,
   delete_cart,
-  get_carts,
-  update_cart
+  delete_cart_item,
+  get_all_details_cart_by_user_id,
+  get_cart,
+  get_cart_items_by_cart_id,
+  update_cart,
+  update_cart_item
 } from "../controllers/cart_controller.js";
 
 const cart_router = express.Router();
-// Cart API routes
-cart_router.get("/", get_carts);
-cart_router.get("/:id", get_carts);
-cart_router.get("/:userId", get_carts);
-cart_router.get("/:type/:userId", get_carts);
 
+// ==================== CART ROUTES ====================
+cart_router.get("/:userId", get_cart);
 cart_router.post("/", create_cart);
+cart_router.put("/:userId", update_cart);
+cart_router.delete("/:id", delete_cart);
+cart_router.delete("/clear/:userId", delete_all_cart);
 
-cart_router.delete("/", delete_cart);
-cart_router.delete("/:id/:auth", delete_cart);
-
-cart_router.put("/", update_cart);
-cart_router.put("/:id", update_cart);
-
+// ==================== CART ITEM ROUTES ====================
+cart_router.get("/items/:userId", get_all_details_cart_by_user_id);
+cart_router.get("/items/cart/:cartId", get_cart_items_by_cart_id);
+cart_router.post("/items/:cartId", create_cart_item); 
+cart_router.put("/items/:id", update_cart_item);
+cart_router.delete("/items/:id", delete_cart_item); // <<< TEST ALL
 
 export { cart_router };
