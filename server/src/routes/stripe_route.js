c94c0b7b6b0678a4f9d703a786bc099e19cc030b
Bannavnog <bannavong.sa239717@sis.hust.edu.vn>
2026-01-05 23:20:15 +0700

    - Add payment handling and cancellation features to checkout process - Stock decrease logic

--- server/src/routes/stripe_route.js
+++ server/src/routes/stripe_route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { getActiveCart } from '../controllers/cart_controller.js';
 import { checkout_middle } from '../controllers/checkout_controller.js';
-import { createCheckoutSession, paymentConfirm } from '../controllers/stripe_controller.js';
+import { createCheckoutSession, paymentConfirm, paymentCancel } from '../controllers/stripe_controller.js';
 import { authMiddleware } from '../middleware/auth.js';
   
 const stripeRouter = express.Router();
 
 stripeRouter.post('/create-checkout-session', authMiddleware, getActiveCart ,checkout_middle, createCheckoutSession);
 stripeRouter.get('/payment-success',  paymentConfirm);
+stripeRouter.get('/payment-cancel',  paymentCancel);
 
 export { stripeRouter };
